7a95157f9c7f39f9fab2b87d576e22d2e8873643
MSNZT <shamyphy@gmail.com>
2026-05-18 02:04:39 +0300

    fix: scan users repository rows

--- internal/users/repository.go
+++ internal/users/repository.go
@@ -2,11 +2,11 @@ package users
 
 import (
 	"context"
-	"database/sql"
 	"errors"
 	"fmt"
 
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -21,7 +21,7 @@ func NewRepository(pool *pgxpool.Pool) *Repository {
 
 var (
 	ErrUserNotFound     = errors.New("user not found")
-	ErrEmailAlreadyUsed = errors.New("user already used")
+	ErrEmailAlreadyUsed = errors.New("email already used")
 )
 
 func (r *Repository) Create(ctx context.Context, user User) error {
@@ -35,7 +35,9 @@ func (r *Repository) Create(ctx context.Context, user User) error {
 	_, err := r.pool.Exec(ctx, query, user.ID, user.Email, user.PasswordHash, user.Role)
 	if err != nil {
 		if errors.As(err, &pgErr) && pgErr.Code == "23505" {
-			return fmt.Errorf("%v: %w", op, ErrEmailAlreadyUsed)
+			if pgErr.ConstraintName == "users_email_key" {
+				return fmt.Errorf("%s: %w", op, ErrEmailAlreadyUsed)
+			}
 		}
 
 		return fmt.Errorf("%s: %w", op, err)
@@ -47,36 +49,46 @@ func (r *Repository) Create(ctx context.Context, user User) error {
 func (r *Repository) GetByEmail(ctx context.Context, email string) (*User, error) {
 	const op = "users.repository.GetByEmail"
 
-	query := "SELECT * FROM users WHERE email = $1"
-	var user User
+	query := `SELECT 
+		id, email, password_hash, role, create_at, updated_at 
+		FROM users 
+		WHERE email = $1`
+	var u User
 
-	err := r.pool.QueryRow(ctx, query, email).Scan(&user)
+	err := r.pool.QueryRow(ctx, query, email).Scan(
+		&u.ID, &u.Email, &u.PasswordHash, &u.Role, &u.CreatedAt, &u.UpdatedAt,
+	)
 	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			return nil, fmt.Errorf("%v: %w", op, ErrUserNotFound)
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, fmt.Errorf("%s: %w", op, ErrUserNotFound)
 		}
 
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
-	return &user, nil
+	return &u, nil
 }
 
 func (r *Repository) GetByID(ctx context.Context, id uuid.UUID) (*User, error) {
 	const op = "users.repository.GetByID"
 
-	query := "SELECT * FROM users WHERE id = $1"
+	query := `SELECT 
+		id, email, password_hash, role, create_at, updated_at 
+		FROM users 
+		WHERE id = $1`
 
-	var user User
+	var u User
 
-	err := r.pool.QueryRow(ctx, query, id).Scan(&user)
+	err := r.pool.QueryRow(ctx, query, id).Scan(
+		&u.ID, &u.Email, &u.PasswordHash, &u.Role, &u.CreatedAt, &u.UpdatedAt,
+	)
 	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
+		if errors.Is(err, pgx.ErrNoRows) {
 			return nil, fmt.Errorf("%v: %w", op, ErrUserNotFound)
 		}
 
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
-	return &user, nil
+	return &u, nil
 }
